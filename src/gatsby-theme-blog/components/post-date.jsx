69d62be9737f5eb623cad765a2435c55133a3068
Jakub Jafra <jakub.jafra@gmail.com>
2021-02-15 23:34:25 +0100

    New blog post & version upgrades

--- src/gatsby-theme-blog/components/post-date.jsx
+++ src/gatsby-theme-blog/components/post-date.jsx
@@ -3,7 +3,6 @@ import { jsx } from 'theme-ui';
 import { parse, format } from 'date-fns';
 
 const PostDate = ({ children }) => {
-  console.log();
   return <small>{format(parse(children, 'MMMM dd, yyyy', new Date()), 'yyyy/MM/dd')}</small>;
 };
 
